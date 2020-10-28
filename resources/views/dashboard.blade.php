bf8a84d3963aa33581e9e321c8b90fb6be866ee8
harrysudana <harrysudana@gmail.com>
2020-10-28 20:09:34 +0800

    include the components CreateLink and Analytics in your dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,21 @@
 <x-app-layout>
     <x-slot name="header">
+        <h2 class="font-semibold text-xl text-center text-gray-800 leading-tight">
+            {{ __('Welcome to URL Shortener') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @livewire('create-link')
+            @livewire('analytics')
+        </div>
+    </div>
+</x-app-layout>
+
+<!--
+<x-app-layout>
+    <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
@@ -13,3 +29,4 @@
         </div>
     </div>
 </x-app-layout>
+-->
