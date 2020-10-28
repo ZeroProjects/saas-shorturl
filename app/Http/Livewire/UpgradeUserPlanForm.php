ad66b85e17b92bc52420ae3994339abc389cdcd2
harrysudana <harrysudana@gmail.com>
2020-10-28 20:00:14 +0800

    % php artisan make:livewire UpgradeUserPlanForm

---
+++ app/Http/Livewire/UpgradeUserPlanForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class UpgradeUserPlanForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.upgrade-user-plan-form');
+    }
+}
