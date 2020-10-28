36ce18c9074a71cb7e846c899a90fe6e3da2490c
harrysudana <harrysudana@gmail.com>
2020-10-28 20:04:55 +0800

    the back-end for the CreateLink component

--- app/Http/Livewire/CreateLink.php
+++ app/Http/Livewire/CreateLink.php
@@ -3,6 +3,55 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class CreateLink extends Component
+{
+    use AuthorizesRequests;
+
+    public $original;
+    protected $linkLength = 6;
+
+    public function submit()
+    {
+        $this->validate(['original' => 'required|url']);
+
+        $this->authorizeCreatingLink();
+
+        auth()->user()->links()->create([
+            'original' => $this->original,
+            'shortened' => $this->generateShortLink(),
+        ]);
+
+        $this->emit('linkCreated');
+        $this->original = '';
+    }
+
+    protected function authorizeCreatingLink()
+    {
+        $user = auth()->user();
+        abort_if($user->plan == 'free' && $user->links()->count() == 3, 403, 'Please upgrade your plan to continue using our service!');
+        abort_if($user->plan == 'premium' && $user->links()->count() == 100, 403, 'Please upgrade your plan to continue using our service!');
+        abort_if($user->links()->where('original', $this->original)->count(), 403, 'You already posted this link before!');
+    }
+
+    protected function generateShortLink()
+    {
+        return substr(md5(time()), 0, $this->linkLength);
+    }
+
+    public function render()
+    {
+        return view('livewire.create-link');
+    }
+}
+
+/*
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
 
 class CreateLink extends Component
 {
@@ -11,3 +60,4 @@ class CreateLink extends Component
         return view('livewire.create-link');
     }
 }
+*/
